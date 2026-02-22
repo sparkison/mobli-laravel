05cc60ecd9a04037d6ca7863d02ae165d631fc27
Shaun Parkison <shparkison@gmail.com>
2026-02-22 12:00:56 -0700

    chore: Update screens and sync options

--- database/migrations/2018_10_10_000000_create_mail_templates_table.php
+++ database/migrations/2018_10_10_000000_create_mail_templates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMailTemplatesTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class CreateMailTemplatesTable extends Migration
             $table->timestamps();
         });
     }
-}
+};
